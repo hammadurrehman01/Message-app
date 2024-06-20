be29e184654413684a6168961d9dc12db5c10570
hammaduurehman01 <hammmadurrehman@techlabs.cc>
2024-06-20 17:08:51 +0500

    feat: add zod for validation

---
+++ src/schemas/signinSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const emailValidation = z
+  .string()
+  .email({ message: "Invalid email address" })
+  .regex(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
+
+export const passwordValidation = z
+  .string()
+  .min(6, "Username must be at least 6 characters");
+
+export const signinSchema = z.object({
+  email: emailValidation,
+  password: passwordValidation,
+});
