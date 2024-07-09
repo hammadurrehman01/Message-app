7e7103c7ab5654913a9ecd311dba2d9df522a941
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-07-10 01:54:34 +0500

    feat: check unique username API

---
+++ src/app/api/check-unique-username/route.ts
@@ -0,0 +1,29 @@
+import dbConnect from "@/lib/dbConnect";
+import { usernameValidation } from "@/schemas/signupSchema";
+import z from "zod";
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request) {
+    await dbConnect();
+
+    try {
+        const { searchParams } = new URL(request.url);
+        const queryParam = {
+            username: searchParams.get("username")
+        }
+    } catch (err) {
+        console.log("Error checking username", err);
+        return Response.json(
+            {
+                success: false,
+                message: "Error checking username"
+            },
+            { status: 500 }
+
+        )
+
+    }
+}
