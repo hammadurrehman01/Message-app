3447638cf87e467459b8a5247bcb0a05ba181aed
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-07-13 01:19:20 +0500

    feat: check-unique-username API

--- src/app/api/check-unique-username/route.ts
+++ src/app/api/check-unique-username/route.ts
@@ -1,4 +1,5 @@
 import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
 import { usernameValidation } from "@/schemas/signupSchema";
 import z from "zod";
 
@@ -11,9 +12,47 @@ export async function GET(request: Request) {
 
     try {
         const { searchParams } = new URL(request.url);
+
         const queryParam = {
             username: searchParams.get("username")
         }
+        
+        const result = UsernameQuerySchema.safeParse(queryParam);
+
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || [];
+
+            return Response.json(
+                {
+                    success: false,
+                    message: usernameErrors.length > 0 ? usernameErrors.join() : "Invalid query parameters"
+                },
+                { status: 400 }
+            )
+        }
+
+        const { username } = result.data;
+
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true });
+
+        if (existingVerifiedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username is already taken"
+                },
+                { status: 400 }
+            )
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Username is available"
+            },
+            { status: 200 }
+        )
+
     } catch (err) {
         console.log("Error checking username", err);
         return Response.json(
